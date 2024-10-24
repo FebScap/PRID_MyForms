2cbf39f1df81730d707f9223212ec6f8f57e93be
Benoît Penelle <benoit@penelle.net>
2024-10-24 14:07:23 +0200

    feat: version initiale

---
+++ backend/Controllers/FormsController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using prid_2425_f02.Helpers;
+using prid_2425_f02.Models;
+
+namespace prid_2425_f02.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class FormsController : ControllerBase {
+    private readonly FormContext _context;
+    private readonly IMapper _mapper;
+    
+    public FormsController(FormContext context, IMapper mapper) {
+        _context = context;
+        _mapper = mapper;
+    }
+    
+    /*
+     
+     Contrôleur pour les forms
+     
+     */
+}
