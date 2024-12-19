a62d109398c129b110a843ffcd819c005ffb831d
Nicolas Vanhorle <vanhorlenicolas@gmail.com>
2024-12-19 13:48:11 +0100

    fix: login for debug using new email adresses

--- backend/Controllers/QuestionController.cs
+++ backend/Controllers/QuestionController.cs
@@ -1,6 +1,14 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using prid_2425_f02.Models;
+
 namespace prid_2425_f02.Controllers
 {
-    public class QuestionController
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController (Context context, IMapper mapper) : ControllerBase
     {
         
     }
--- backend/Controllers/UsersController.cs
+++ backend/Controllers/UsersController.cs
@@ -1,5 +1,4 @@
 using AutoMapper;
-using AutoMapper.Execution;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
